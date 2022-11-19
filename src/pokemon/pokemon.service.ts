33b011b676263d9f28be0d2f9835c8d067bf143d
Rodrigo Guerrero <roguerrero.go@gmail.com>
2022-11-19 17:45:00 -0300

    feat: se crea pokemon y se controla el error en service

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,12 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
-    return createPokemonDto;
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if(error.code === 11000) {
+        throw new BadRequestException(`Pokemon exist in data base: ${JSON.stringify(error.keyValue)}`)
+      }
+      console.error(error);
+      throw new InternalServerErrorException(`Can't create Pokemon - check logs`);
+    }
   }
 
   findAll() {
